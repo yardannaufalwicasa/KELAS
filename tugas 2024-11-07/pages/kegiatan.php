48e0e4adef164b21823e37529a880b4491b637c2
yardan <yardannaufal150807@gmail.com>
2024-11-08 04:38:49 -0800

    Add files via upload

---
+++ tugas 2024-11-07/pages/kegiatan.php
@@ -0,0 +1,4 @@
+<div class="kegiatan">
+    <h2>Kegiatan</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit tenetur ipsam numquam cumque, voluptatum illo mollitia laudantium consequuntur deserunt ullam architecto omnis a, dolorum, ut dicta ipsum similique eligendi fuga.</p>
+</div>
---
+++ tugas 2024-11-07/pages/sejarah.php
@@ -0,0 +1,4 @@
+<div class="sejarah">
+    <h2>Sejarah</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel inventore dolorum aut saepe maxime excepturi et corporis! Eaque aut quam modi adipisci, dolorem ab corporis aliquid sequi vero quia laboriosam!</p>
+</div>
