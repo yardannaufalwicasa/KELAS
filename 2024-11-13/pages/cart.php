1f2c7b40a8dc6af1ac1558d7fabbe9a1d1ab7b11
yardan <yardannaufal150807@gmail.com>
2024-11-15 07:27:10 -0800

    Add files via upload

---
+++ 2024-11-13/pages/cart.php
@@ -0,0 +1,8 @@
+<div class="cart">
+    <h1>Cart</h1>
+    <form action="" method="post">
+        <input type="email" name="email" required placeholder="Masukan email">
+        <input type="password" name="password" require placeholder="Masukkan password">
+        <input type="submit" name="kirim" value="kirim">
+    </form>
+</div>
